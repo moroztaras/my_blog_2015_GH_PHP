3b5aa8bb0c1b841674171942a85874e93ad761e5
moroztaras <moroztaras@i.ua>
2015-01-06 15:49:31 +0200

    get all the records from the database

--- src/Moroz/BlogBundle/Controller/PostController.php
+++ src/Moroz/BlogBundle/Controller/PostController.php
@@ -3,11 +3,14 @@
 namespace Moroz\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MorozBlogBundle:Post:index.html.twig');
+        $manager=$this->getDoctrine()->getManager();
+        $posts=$this->getDoctrine()->getRepository('BlogBlogBundle:Post')->findBy([],['id'=>'DESC']);
+        return $this->render('BlogBlogBundle:Post:index.html.twig', array('posts'=>$posts));
     }
 }
