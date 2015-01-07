294aeb873cba88bbfae30967b3bf50f40c4d3bf1
moroztaras <moroztaras@i.ua>
2015-01-07 16:59:00 +0200

    add code in metod - views

--- src/Moroz/BlogBundle/Controller/PostController.php
+++ src/Moroz/BlogBundle/Controller/PostController.php
@@ -2,7 +2,10 @@
 
 namespace Moroz\BlogBundle\Controller;
 
+use Moroz\BlogBundle\Entity\Post;
+use Moroz\BlogBundle\Form\Type\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
@@ -16,7 +19,30 @@ class PostController extends Controller
 
     public function showAction($id)
     {
-        $post=$this->getDoctrine()->getRepository('MorozBlogBundle:Post')->find($id);
+        $manager=$this->getDoctrine()->getManager();
+        $post=$manager->getRepository('MorozBlogBundle:Post')->find($id);
+        $post->setCounter($post->getCounter()+1);
+        $manager->flush();
         return $this->render('MorozBlogBundle:Post:show.html.twig', array('post'=>$post));
     }
+
+    public function newAction(Request $request)
+    {
+        $post= new Post();
+        $post->setPostedDateToCurrent();
+
+        $form=$this->createForm(new PostType(),$post);
+        $form->handleRequest($request);
+
+        if ($form->isValid()){
+            $maneger=$this->getDoctrine()->getManager();
+            $post->setCounter();
+            $maneger->persist($post);
+            $maneger->flush();
+
+            return $this->redirect(($this->generateUrl('blog_homepage')));
+        }else{
+            return $this->render('MorozBlogBundle:Post:new.html.twig',array('form'=>$form->createView()));
+        }
+   }
 }
